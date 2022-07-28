0cf5ba7ce16bc26f341c2512fb6ab55e7b3dc0e0
Daniel Castello <castello.daniel@hotmail.com>
2022-07-28 18:05:11 -0300

    configuracao do multipart/form-data

--- src/functions/auth.ts
+++ src/functions/auth.ts
@@ -4,36 +4,50 @@ import type {Handler, APIGatewayEvent} from 'aws-lambda';
 import { emailRegex, passwordRegex } from '../constants/Regexes';
 import { UserRegisterRequest } from '../types/auth/UserRegisterRequest';
 import { DefaultJsonResponse, formatDefaultResponse } from '../utils/formatResponsUtil';
+import { User } from '../types/models/User';
+import { UserModel } from '../models/UserModel';
+import { parse } from 'aws-multipart-parser';
 
 export const register : Handler = async(event: APIGatewayEvent) 
     : Promise<DefaultJsonResponse> => {
     try{
-        const {USER_POOL_ID, USER_POOL_CLIENT_ID} = process.env;
+        const {USER_POOL_ID, USER_POOL_CLIENT_ID, USER_TABLE} = process.env;
         if(!USER_POOL_ID || !USER_POOL_CLIENT_ID){
             return formatDefaultResponse(500, 'ENVs do Cognito não encontradas.');
+        }
+
+        if(!USER_TABLE){
+            return formatDefaultResponse(500, 'ENVs da tabela de usuario do dynamo não encontrada.');
         }
 
         if(!event.body){
             return formatDefaultResponse(400, 'Parâmetros de entrada inválidos');
         }
 
-        const request = JSON.parse(event.body) as UserRegisterRequest;
-        const {name, password, email} = request;
+        const formData = parse(event, true);
+        console.log('formData', formData);
 
-        if(!email || !email.match(emailRegex)){
-            return formatDefaultResponse(400, 'Email inválido');
-        }
+        // if(!email || !email.match(emailRegex)){
+        //     return formatDefaultResponse(400, 'Email inválido');
+        // }
 
-        if(!password || !password.match(passwordRegex)){
-            return formatDefaultResponse(400, 'Senha inválida');
-        }
+        // if(!password || !password.match(passwordRegex)){
+        //     return formatDefaultResponse(400, 'Senha inválida');
+        // }
 
-        if(!name || name.trim().length < 2){
-            return formatDefaultResponse(400, 'Nome inválido');
-        }
+        // if(!name || name.trim().length < 2){
+        //     return formatDefaultResponse(400, 'Nome inválido');
+        // }
 
-        await new CognitoServices(USER_POOL_ID, USER_POOL_CLIENT_ID).signUp(email, password);
+        // const cognitoUser = await new CognitoServices(USER_POOL_ID, USER_POOL_CLIENT_ID).signUp(email, password);
 
+        // const user = {
+        //     name,
+        //     email,
+        //     cognitoId: cognitoUser.userSub
+        // } as User;
+
+        // await UserModel.create(user);
         return formatDefaultResponse(200, 'Usuário cadastrado com sucesso!');
     }catch(error){
         console.log('Error on register user:', error);
