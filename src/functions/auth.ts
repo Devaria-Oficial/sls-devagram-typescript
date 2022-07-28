ddb43fce4b6066f6ffa8b221f3920a8937322611
Daniel Castello <castello.daniel@hotmail.com>
2022-07-28 19:09:11 -0300

    conclusao da integracao com s3

--- src/functions/auth.ts
+++ src/functions/auth.ts
@@ -1,17 +1,19 @@
+import { S3Service } from './../services/S3Services';
 import { ConfirmEmailRequest } from './../types/auth/ConfirmEmailRequest';
 import { CognitoServices } from './../services/CognitoServices';
 import type {Handler, APIGatewayEvent} from 'aws-lambda';
-import { emailRegex, passwordRegex } from '../constants/Regexes';
+import { emailRegex, imageExtensionsAllowed, passwordRegex } from '../constants/Regexes';
 import { UserRegisterRequest } from '../types/auth/UserRegisterRequest';
 import { DefaultJsonResponse, formatDefaultResponse } from '../utils/formatResponsUtil';
 import { User } from '../types/models/User';
 import { UserModel } from '../models/UserModel';
 import { parse } from 'aws-multipart-parser';
+import { FileData } from 'aws-multipart-parser/dist/models';
 
 export const register : Handler = async(event: APIGatewayEvent) 
     : Promise<DefaultJsonResponse> => {
     try{
-        const {USER_POOL_ID, USER_POOL_CLIENT_ID, USER_TABLE} = process.env;
+        const {USER_POOL_ID, USER_POOL_CLIENT_ID, USER_TABLE, AVATAR_BUCKET} = process.env;
         if(!USER_POOL_ID || !USER_POOL_CLIENT_ID){
             return formatDefaultResponse(500, 'ENVs do Cognito não encontradas.');
         }
@@ -20,34 +22,51 @@ export const register : Handler = async(event: APIGatewayEvent)
             return formatDefaultResponse(500, 'ENVs da tabela de usuario do dynamo não encontrada.');
         }
 
+        if(!AVATAR_BUCKET){
+            return formatDefaultResponse(500, 'ENVs do bucket de avatar não encontrada.');
+        }
+
         if(!event.body){
             return formatDefaultResponse(400, 'Parâmetros de entrada inválidos');
         }
 
         const formData = parse(event, true);
-        console.log('formData', formData);
+        const file = formData.file as FileData;
+        const name = formData.name as string;
+        const email = formData.email as string;
+        const password = formData.password as string;
 
-        // if(!email || !email.match(emailRegex)){
-        //     return formatDefaultResponse(400, 'Email inválido');
-        // }
+        if(!email || !email.match(emailRegex)){
+            return formatDefaultResponse(400, 'Email inválido');
+        }
 
-        // if(!password || !password.match(passwordRegex)){
-        //     return formatDefaultResponse(400, 'Senha inválida');
-        // }
+        if(!password || !password.match(passwordRegex)){
+            return formatDefaultResponse(400, 'Senha inválida');
+        }
 
-        // if(!name || name.trim().length < 2){
-        //     return formatDefaultResponse(400, 'Nome inválido');
-        // }
+        if(!name || name.trim().length < 2){
+            return formatDefaultResponse(400, 'Nome inválido');
+        }
 
-        // const cognitoUser = await new CognitoServices(USER_POOL_ID, USER_POOL_CLIENT_ID).signUp(email, password);
+        if(file && !imageExtensionsAllowed.exec(file.filename)){
+            return formatDefaultResponse(400, 'Extensão informada do arquivo não é válida');
+        }
 
-        // const user = {
-        //     name,
-        //     email,
-        //     cognitoId: cognitoUser.userSub
-        // } as User;
+        const cognitoUser = await new CognitoServices(USER_POOL_ID, USER_POOL_CLIENT_ID).signUp(email, password);
 
-        // await UserModel.create(user);
+        let key = undefined;
+        if(file){
+            key = await new S3Service().saveImage(AVATAR_BUCKET, 'avatar', file);
+        }
+
+        const user = {
+            name,
+            email,
+            cognitoId: cognitoUser.userSub,
+            avatar: key
+        } as User;
+
+        await UserModel.create(user);
         return formatDefaultResponse(200, 'Usuário cadastrado com sucesso!');
     }catch(error){
         console.log('Error on register user:', error);
