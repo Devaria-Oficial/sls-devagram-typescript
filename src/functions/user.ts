cbfeede25c2aa4f77c03b48beaf9eebf502dc06b
Daniel Castello <castello.daniel@hotmail.com>
2022-07-31 11:53:13 -0300

    conclusao edicao de dados do usuario

--- src/functions/user.ts
+++ src/functions/user.ts
@@ -1,8 +1,11 @@
+import { imageExtensionsAllowed } from './../constants/Regexes';
 import { S3Service } from './../services/S3Services';
 import { UserModel } from './../models/UserModel';
 import { getUserIdFromEvent } from './../utils/authenticationHandlerUtils';
 import { APIGatewayEvent, Handler } from "aws-lambda";
 import { DefaultJsonResponse, formatDefaultResponse } from "../utils/formatResponsUtil";
+import { parse } from 'aws-multipart-parser';
+import { FileData } from 'aws-multipart-parser/dist/models';
 
 export const me: Handler = async (event: APIGatewayEvent):
     Promise<DefaultJsonResponse> => {
@@ -27,5 +30,45 @@ export const me: Handler = async (event: APIGatewayEvent):
     } catch (error) {
         console.log('Error on login user:', error);
         return formatDefaultResponse(500, 'Erro ao autenticar usuario! Tente novamente ou contacte o administrador do sistema.');
+    }
+}
+
+export const update : Handler = async(event : APIGatewayEvent) :
+    Promise<DefaultJsonResponse> =>{
+    try{
+        const {USER_TABLE,AVATAR_BUCKET} = process.env;
+        if (!USER_TABLE || !AVATAR_BUCKET) {
+            return formatDefaultResponse(500, 'ENVs para servico não encontradas.');
+        }
+
+        const userId = getUserIdFromEvent(event);
+        if(!userId){
+            return formatDefaultResponse(400, 'Usuário não encontrado.');
+        }
+
+        const user = await UserModel.get({'cognitoId' : userId});
+
+        const formData = parse(event, true);
+        const file = formData.file as FileData;
+        const name = formData.name as string;
+
+        if(name && name.trim().length < 2){
+            return formatDefaultResponse(400, 'Nome inválido');
+        }else if(name){
+            user.name = name;
+        }
+
+        if(file && !imageExtensionsAllowed.exec(file.filename)){
+            return formatDefaultResponse(400, 'Extensão informada do arquivo não é válida');
+        }else if(file){
+            const newKey = await new S3Service().saveImage(AVATAR_BUCKET, 'avatar', file);
+            user.avatar = newKey;
+        }
+
+        await UserModel.update(user);
+        return formatDefaultResponse(200, 'Usuário alterado com sucesso!');
+    }catch(error){
+        console.log('Error on update user data:', error);
+        return formatDefaultResponse(500, 'Erro ao atualizar dados do usuario! Tente novamente ou contacte o administrador do sistema.');
     }
 }
