d8fb01fe68eab2c2b992eb68a1ee283d1c3282fe
Daniel Castello <castello.daniel@hotmail.com>
2022-08-02 22:04:53 -0300

    conclusao do endpoint de comentario

--- src/functions/post.ts
+++ src/functions/post.ts
@@ -11,9 +11,9 @@ import * as Uuid from 'uuid';
 import * as moment from 'moment';
 import { PostModel } from '../models/PostModel';
 
-export const create : Handler = async(event : APIGatewayEvent) :
-    Promise<DefaultJsonResponse> =>{
-    try{
+export const create: Handler = async (event: APIGatewayEvent):
+    Promise<DefaultJsonResponse> => {
+    try {
         const { POST_BUCKET, error } = validateEnvs(['POST_TABLE',
             'POST_BUCKET']);
         if (error) {
@@ -21,12 +21,12 @@ export const create : Handler = async(event : APIGatewayEvent) :
         }
 
         const userId = getUserIdFromEvent(event);
-        if(!userId){
+        if (!userId) {
             return formatDefaultResponse(400, 'Usuário não encontrado.');
         }
 
-        const user = await UserModel.get({'cognitoId' : userId});
-        if(!user){
+        const user = await UserModel.get({ 'cognitoId': userId });
+        if (!user) {
             return formatDefaultResponse(400, 'Usuário não encontrado.');
         }
 
@@ -34,16 +34,16 @@ export const create : Handler = async(event : APIGatewayEvent) :
         const file = formData.file as FileData;
         const description = formData.description as string;
 
-        if(!description || description.trim().length < 5){
+        if (!description || description.trim().length < 5) {
             return formatDefaultResponse(400, 'Descrição inválida');
         }
 
-        if(!file || !imageExtensionsAllowed.exec(file.filename)){
+        if (!file || !imageExtensionsAllowed.exec(file.filename)) {
             return formatDefaultResponse(400, 'Extensão informada do arquivo não é válida');
         }
 
         const imageKey = await new S3Service().saveImage(POST_BUCKET, 'post', file);
-        
+
         const post = {
             id: Uuid.v4(),
             userId,
@@ -53,36 +53,36 @@ export const create : Handler = async(event : APIGatewayEvent) :
         };
 
         await PostModel.create(post);
-        user.posts = user.posts+1;
+        user.posts = user.posts + 1;
         await UserModel.update(user);
         return formatDefaultResponse(200, 'Publicação criada com sucesso!');
-    }catch(error){
+    } catch (error) {
         console.log('Error on create post:', error);
         return formatDefaultResponse(500, 'Erro ao criar publicação! Tente novamente ou contacte o administrador do sistema.');
     }
 }
 
-export const toggleLike : Handler = async(event : any) :
-    Promise<DefaultJsonResponse> =>{
-    try{
+export const toggleLike: Handler = async (event: any):
+    Promise<DefaultJsonResponse> => {
+    try {
         const { error } = validateEnvs(['POST_TABLE']);
         if (error) {
             return formatDefaultResponse(500, error);
         }
 
         const userId = getUserIdFromEvent(event);
-        if(!userId){
+        if (!userId) {
             return formatDefaultResponse(400, 'Usuário não encontrado.');
         }
 
-        const user = await UserModel.get({'cognitoId' : userId});
-        if(!user){
+        const user = await UserModel.get({ 'cognitoId': userId });
+        if (!user) {
             return formatDefaultResponse(400, 'Usuário não encontrado.');
         }
 
-        const {postId} = event.pathParameters;
-        const post = await PostModel.get({id : postId});
-        if(!post){
+        const { postId } = event.pathParameters;
+        const post = await PostModel.get({ id: postId });
+        if (!post) {
             return formatDefaultResponse(400, 'Publicação não encontrada.');
         }
 
@@ -91,17 +91,63 @@ export const toggleLike : Handler = async(event : any) :
             return result;
         });
 
-        if(hasLikedIndex != -1){
+        if (hasLikedIndex != -1) {
             post.likes.splice(hasLikedIndex, 1);
             await PostModel.update(post);
             return formatDefaultResponse(200, 'Like removido com sucesso!');
-        }else{
+        } else {
             post.likes.push(userId);
             await PostModel.update(post);
             return formatDefaultResponse(200, 'Like adicionado com sucesso!');
         }
-    }catch(error){
+    } catch (error) {
         console.log('Error on toggle like:', error);
         return formatDefaultResponse(500, 'Erro ao curtir/descurtir a publicação! Tente novamente ou contacte o administrador do sistema.');
+    }
+}
+
+export const postComent: Handler = async (event: any): Promise<DefaultJsonResponse> => {
+    try {
+        const { error } = validateEnvs(['POST_TABLE']);
+        if (error) {
+            return formatDefaultResponse(500, error);
+        }
+
+        const userId = getUserIdFromEvent(event);
+        if (!userId) {
+            return formatDefaultResponse(400, 'Usuário não encontrado.');
+        }
+
+        const user = await UserModel.get({ 'cognitoId': userId });
+        if (!user) {
+            return formatDefaultResponse(400, 'Usuário não encontrado.');
+        }
+
+        const { postId } = event.pathParameters;
+        const post = await PostModel.get({ id: postId });
+        if (!post) {
+            return formatDefaultResponse(400, 'Publicação não encontrada.');
+        }
+
+        const request = JSON.parse(event.body);
+        const {coment } = request;
+
+        if(!coment || coment.length < 2){
+            return formatDefaultResponse(400, 'Comentário não é válido.');
+        }
+
+        const comentObj = {
+            userId,
+            userName: user.name,
+            date: moment().format(),
+            coment
+        }
+
+        post.coments.push(comentObj);
+        await PostModel.update(post);
+        return formatDefaultResponse(200, 'Comentário adicionado com sucesso!');
+    } catch (error) {
+        console.log('Error on post comment like:', error);
+        return formatDefaultResponse(500, 'Erro ao comentar na publicação! Tente novamente ou contacte o administrador do sistema.');
     }
 }
